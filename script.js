a1134401e4405b560ef769a9d114f034fad5cc80
Yuxin Zeng <yzeng0521@gmail.com>
2025-05-22 14:50:03 -0400

    separate files

---
+++ script.js
@@ -0,0 +1,47 @@
+const bruh = new Audio();
+bruh.src = "./sounds/bruh_sfx.mp3"
+
+const vine_boom = new Audio();
+vine_boom.src = "./sounds/vine_boom_sfx.mp3"
+
+const huh = new Audio();
+huh.src = "./sounds/huh_sfx.mp3"
+
+const rawhr = new Audio();
+rawhr.src = "./sounds/rahr_sfx.mp3"
+
+const dissapointed = new Audio();
+dissapointed.src = "./sounds/spongebob_dissapointed_sfx.mp3"
+
+const disgusted = new Audio();
+disgusted.src = "./sounds/spongebob_disgusted_sfx.mp3"
+
+const suspense = new Audio();
+suspense.src = "./sounds/suspense_sfx.mp3"
+
+const alarm = new Audio();
+alarm.src = "./sounds/alarm_sfx.mp3"
+
+const boy_what_the_hell = new Audio();
+boy_what_the_hell.src = "./sounds/boy_what_the_hell_boy_sfx.mp3"
+
+const gah_damn = new Audio();
+gah_damn.src = "./sounds/gah_damn_sfx.mp3"
+
+const yippee = new Audio();
+yippee.src = "./sounds/yippee_sfx.mp3"
+
+const cheering = new Audio();
+cheering.src = "./sounds/kids_cheering_sfx.mp3"
+
+const laughing = new Audio();
+laughing.src = "./sounds/dog_laughing_sfx.mp3"
+
+const alien = new Audio();
+alien.src = "./sounds/the_alien_sfx.mp3"
+
+const oh_hell_nah = new Audio();
+oh_hell_nah.src = "./sounds/omg_oh_hell_nah_sfx.mp3"
+
+const nerd_emoji = new Audio();
+nerd_emoji.src = "./sounds/nerd_emoji_sfx.mp3"
